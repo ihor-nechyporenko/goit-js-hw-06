ae2aaa4f58fce525a24f0aeb32faad6de44f3e04
Ihor Nechyporenko <nechyporenko.i@gmail.com>
2021-02-10 14:57:09 +0200

    add tasks 26-34

---
+++ js/task-30.js
@@ -0,0 +1,23 @@
+const players = {
+    mango: 1270,
+    poly: 468,
+    ajax: 710,
+    kiwi: 244
+};
+const playtimes = Object.values(players); // [1270, 468, 710, 244]
+// Пиши код ниже этой строки
+  
+const totalPlayTime = playtimes.reduce((total, playtime) => {
+    return total + playtime;
+}, 0);
+  
+// Пиши код выше этой строки
+const averagePlayTime = totalPlayTime / playtimes.length;
+
+console.log(totalPlayTime);
+console.log(averagePlayTime);
+
+// ------------------------------------------------------------------------------------------
+
+// Игровому сервису необходим функционал подсчёта среднего времени проведённого в играх. Дополни код так, 
+// чтобы в переменной totalPlayTime получилось общее игровое время из массива playtimes.
